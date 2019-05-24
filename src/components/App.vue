fc88654cb81f76012d58dda1c0a87bda17fb4eef
Achhunna Mali <achhunna@gmail.com>
2019-05-24 07:21:31 -0700

    padding button and adding event message

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="main-container">
-        <child>I am a child. My name is {{ name }}. This is my {{ value }}.</child>
+        <child>
+            I am a child. My name is {{ name }}. This is my {{ value }}.
+        </child>
+        <transition name="fade">
+            <div v-if="event">You dispatched {{ event }} event.</div>
+        </transition>
         <button class="close-btn" @click="handleCloseBtnClick">Close</button>
         <button class="search-btn" @click="handleSearchBtnClick">Search</button>
     </div>
@@ -19,6 +24,7 @@ export default {
         return {
             name: Constants.name,
             value: Constants.value,
+            event: null,
         }
     },
     methods: {
@@ -34,10 +40,29 @@ export default {
         asyncFunction() {
             setTimeout(() => this.$emit('asyncComplete'), 1000);
         },
-    }
+        resetEvent() {
+            setTimeout(() => this.event = null, 3000);
+        },
+    },
+    created() {
+        this.$on('close', () => {
+            this.event = 'close';
+            this.resetEvent();
+        });
+        this.$on('search', () => {
+            this.event = 'search';
+            this.resetEvent();
+        });
+    },
 };
 </script>
 
 <style lang="scss" scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
 </style>
 
